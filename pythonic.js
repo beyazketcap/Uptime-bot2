64e1d67049e71e7e6ac16936f6ba45f356a05941
beyazketcap <83273383+beyazketcap@users.noreply.github.com>
2021-04-27 13:22:22 +0300

    Add files via upload

---
+++ pythonic.js
@@ -0,0 +1,119 @@
+const db = require("quick.db");
+const discord = require("discord.js");
+const client = new discord.Client({ disableEveryone: true });
+const fetch = require("node-fetch");
+const fs = require("fs");
+require("express")().listen(1343);
+
+//UPTİME
+
+const express = require("express");
+const app = express();
+const http = require("http");
+app.get("/", (request, response) => {
+  console.log(Date.now() + "7/24 AKTİF TUTMA İŞLEMİ BAŞARILI");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+var sesese = "2";
+
+const log = message => {
+    console.log(`${message}`);
+};
+
+//OYNUYOR KISMI
+
+client.on("ready", () => {
+  console.log("Bot Aktif");
+  let playing = client.voice.connections.size;
+
+  client.user.setPresence({
+    activity: {
+      name: "Assassins",
+      type: "WATCHING",
+      url: "URL"
+    }
+  });
+});
+
+setInterval(() => {
+  var links = db.get("linkler");
+  if (!links) return;
+  var linkA = links.map(c => c.url);
+  linkA.forEach(link => {
+    try {
+      fetch(link);
+    } catch (e) {
+      console.log("" + e);
+    }
+  });
+  console.log("Pinglendi.");
+}, 60000);
+
+client.on("ready", () => {
+  if (!Array.isArray(db.get("linkler"))) {
+    db.set("linkler", []);
+  }
+});
+
+//embed hazırlıkları
+
+const help = new discord.MessageEmbed()
+.setFooter("Assassins uptime yardımcısı")
+.setColor("RED")
+.setThumbnail('https://i.imgur.com/4M7IWwP.gif')
+.setDescription(`Selamlar, botunu uptime etmeye hazırmısın? \n artık kolay bir şekilde botunu 7/24 aktif edebilirsin! \n\n🤹 uptime olmak için \`!ekle [glitch linki]\` yazabilirsin \n🎭 Uptime ettiğin botlarımı görmek istiyorsun \`!göster\` `)
+
+
+
+
+
+
+
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "!ekle") {
+    var link = spl[1];
+    fetch(link)
+      .then(() => {
+        if (
+          db
+            .get("linkler")
+            .map(z => z.url)
+            .includes(link)
+        )
+             return message.channel.send(new discord.MessageEmbed().setFooter("Assassins-uptime").setColor("RED").setDescription("Projeniz Sistemimizde Zaten Var"));
+        message.channel.send(new discord.MessageEmbed().setFooter("Assassins-uptime").setColor("RED").setDescription("Projeniz Sistemimize Başarıyla Eklendi."));
+        db.push("linkler", { url: link, owner: message.author.id });
+      })
+      .catch(e => {
+        return message.channel.send(new discord.MessageEmbed().setFooter("Assassins-uptime").setColor("RED").setDescription("Lütfen Bir Link Giriniz"));
+      });
+  }
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "!göster") {
+    var link = spl[1];
+    message.channel.send(new discord.MessageEmbed().setFooter("Assassins-uptime").setColor("RED").setDescription(`${db.get("linkler").length} Proje Aktif Tutuluyor!`));
+  }
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "!yardım") {
+    var link = spl[1];
+    message.channel.send(help);
+  }
+});
+
+client.login("ODExNjM4NTE3MjAxMjQwMDk0.YC1HRg.FlkIzv0CkUJ8L8N51mdZ0n-whiw");
